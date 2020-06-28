08173e04667de62a794d6f04147d1ac9c09d0322
Ahana <ahanasi@seas.upenn.edu>
2020-06-28 13:00:46 -0700

    Fix format

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,15 @@
 require_relative "node.rb"
 require "pry"
+
 class Board
   NUMBERS = (0..7).to_a
+
   def initialize()
-    @board = Array.new(64){Node.new(nil,nil,nil)}
+    @board = Array.new(64) { Node.new(nil, nil, nil) }
     setup_board
   end
 
   def setup_board
-    NUMBERS.product(NUMBERS).zip(@board) { |a,b| b.value = a }
+    NUMBERS.product(NUMBERS).zip(@board) { |a, b| b.value = a }
   end
-  
 end
-
-test = Board.new()
--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -1,26 +1,23 @@
 require_relative "board.rb"
 require_relative "node.rb"
+
 class KnightTravails
+  MOVES = [[[2, 1], [2, -1], [-2, 1], [-2, -1]], [[1, 2], [-1, 2], [1, -2], [-1, -2]]]
 
-    MOVES = [[[2, 1], [2, -1], [-2, 1], [-2, -1]],[[1,2], [-1, 2], [1,-2], [-1,-2]]]
-      
   def knight_moves(start_pos, end_pos)
-
   end
 
-  def possible_moves(position=[3,3])
+  def possible_moves(position)
     legal_moves = []
     MOVES.each do |moves|
       moves.map do |arr|
         arr[0] += position[0]
         arr[1] += position[1]
-        legal_moves << arr if (arr.all?{|val| val.between?(0,7)})   
+        legal_moves << arr if (arr.all? { |val| val.between?(0, 7) })
       end
     end
-    legal_moves    
+    legal_moves
   end
-
-
 end
 
 test = KnightTravails.new()
