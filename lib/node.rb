6179b17ebba921f7d880e070431ab26ab189c2b3
Ahana <ahanasi@seas.upenn.edu>
2020-06-28 13:49:09 -0700

    Add default value for parent and children

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 class Node
   attr_accessor :value, :parent, :children
 
-  def initialize(value, parent, children)
+  def initialize(value, parent = nil, children = [])
     @value = value
     @parent = parent
     @children = children
