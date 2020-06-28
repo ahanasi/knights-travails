864a0bcbba205378e6dd44d24f977902caf9d046
Ahana <ahanasi@seas.upenn.edu>
2020-06-28 15:22:56 -0700

    Fix bug in possible_moves which modified MOVES_1, MOVES_2

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -1,24 +1,55 @@
 require_relative "board.rb"
 require_relative "node.rb"
+require "pry"
 
 class KnightTravails
-  MOVES = [[[2, 1], [2, -1], [-2, 1], [-2, -1]], [[1, 2], [-1, 2], [1, -2], [-1, -2]]]
+  attr_accessor :board
+  MOVES_1 = [[2, 1], [2, -1], [-2, 1], [-2, -1]]
+  MOVES_2 = [[1, 2], [-1, 2], [1, -2], [-1, -2]]
 
-  def knight_moves(start_pos, end_pos)
+  def initialize()
+    @board = Board.new().board
+  end
+
+  def knight_moves(start_pos = [0, 0], end_pos = [3, 3])
+    queue = []
+    queue << Node.new(start_pos)
+
+    until queue.empty?
+      current = queue[0]
+      binding.pry
+      current.children << possible_moves(current.value).map { |arr| binding.pry; Node.new(arr, current) }
+      current.children.flatten!
+      binding.pry
+      if current.children.any? { |node| node.value == end_pos }
+        break
+      else
+        queue << current.children
+        queue.flatten!
+        binding.pry
+        queue.shift
+      end
+    end
+    binding.pry
   end
 
   def possible_moves(position)
-    legal_moves = []
-    MOVES.each do |moves|
-      moves.map do |arr|
-        arr[0] += position[0]
-        arr[1] += position[1]
-        legal_moves << arr if (arr.all? { |val| val.between?(0, 7) })
-      end
+    moves = []
+    
+    MOVES_1.each do |arr|
+      moves << position.map.with_index { |val, i| val += arr[i] }
     end
-    legal_moves
+
+    MOVES_2.each do |arr|
+      moves << position.map.with_index { |val, i| val += arr[i] }
+    end
+    moves
+  end
+
+  def valid_moves(all_moves)
+    all_moves.select { |arr| arr if (arr.all? { |val| (val >= 0 && val < 8) }) }
   end
 end
 
 test = KnightTravails.new()
-p test.possible_moves
+binding.pry
