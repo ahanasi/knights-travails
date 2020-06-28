228b0a744e8000f93b072c6a003a99b9c1a52f34
Ahana <ahanasi@seas.upenn.edu>
2020-06-28 11:35:27 -0700

    Create board setup function on initialize

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+require_relative "node.rb"
+require "pry"
+class Board
+  NUMBERS = (0..7).to_a
+  def initialize()
+    @board = Array.new(64){Node.new(nil,nil,nil)}
+    setup_board
+  end
+
+  def setup_board
+    NUMBERS.product(NUMBERS).zip(@board) { |a,b| b.value = a }
+  end
+  
+end
+
+test = Board.new()
